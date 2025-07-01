b7c2aede94ca0f12ff97c764f4785fd83c1e3be5
Anny Albarran <anny.albarran@ixcomercio.com>
2025-07-01 15:48:30 +0000

    Rollback Astro confi

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ export default defineConfig({
   // vite: {
   //   plugins: [tailwindcss()]
   // },
-  output: 'static',
+  output: 'server',
 
   integrations: [tailwindcss(), react(), icon()],
 
