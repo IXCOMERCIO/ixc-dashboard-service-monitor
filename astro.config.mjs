41db114c14243ff446868b6d3ae525ec0efacc51
Anny Albarran <anny.albarran@ixcomercio.com>
2025-07-01 16:42:19 +0000

    Update file astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,18 @@
 // @ts-check
-import { defineConfig , envField} from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@astrojs/tailwind';
-
-//import vercel from '@astrojs/vercel';
-
 import icon from 'astro-icon';
 
-// https://astro.build/config
 export default defineConfig({
-  // vite: {
-  //   plugins: [tailwindcss()]
-  // },
   output: 'static',
 
   integrations: [tailwindcss(), react(), icon()],
 
-  env:{
-    schema:{
-     SHOW_GENERAL_STATUS: envField.boolean({default:true,context:'server',access:'secret'}),
-     BASE_DASHBOARD_URL: envField.string({context:'server',access:'secret'})
+  env: {
+    schema: {
+      SHOW_GENERAL_STATUS: envField.boolean({ default: true, context: 'server', access: 'secret' }),
+      BASE_DASHBOARD_URL: envField.string({ context: 'server', access: 'secret' })
     }
   }
-
-  //adapter: vercel(),
 });
