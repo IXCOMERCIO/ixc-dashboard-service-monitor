82bffc564b815d985854d86945e5cdeaca0f9af1
Jeremy León <jeremy.leon@ixcomercio.com>
2025-08-07 19:04:19 -0500

    UPD: Deploy con deno v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,14 @@
 // @ts-check
-import { defineConfig, envField } from 'astro/config';
+import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@astrojs/tailwind';
 import icon from 'astro-icon';
 
-import node from '@astrojs/node';
+import deno from '@astrojs/deno';
 
 export default defineConfig({
   output: 'server',
   integrations: [tailwindcss(), react(), icon()],
 
-  env: {
-    schema: {
-      SHOW_GENERAL_STATUS: envField.boolean({ default: true, context: 'server', access: 'secret' }),
-      BASE_DASHBOARD_URL: envField.string({ context: 'server', access: 'secret' })
-    }
-  },
-
-  adapter: node({mode: 'standalone'}),
+  adapter: deno(),
 });
